2b06afbcfd1114b54088e2a514034a3ccdb011ed
purushothamsuriya <purushothamdpm@gmail.com>
2024-10-19 07:57:08 +0530

    Update input_output.py

--- input_output.py
+++ input_output.py
@@ -6,8 +6,10 @@
 #print("addition:",a+b,"substraction:",a-b,"multification:",a*b,"division:",a/b)
 #num1=5
 #num2=10
-#print("Sum:",num1+num2,sep="")
+#print("Sum:",num1+num2,sep="") 
+
 num1=int(input())
 num2=int(input())
-#print("Sum:",num1+num2,sep="")
+print("Sum:",num1+num2,sep="")
+     or(with f string)
 print(f"Sum:{num1+num2}")
